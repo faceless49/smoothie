01defc12820ac1a8b0155732f7bdba9573d3b57d
faceless49 <53124783+faceless49@users.noreply.github.com>
2020-12-30 16:44:37 +0300

    Have some bug js script

--- js/script.js
+++ js/script.js
@@ -1,17 +1,27 @@
 window.addEventListener('DOMContentLoaded', () => {
-    const menu = document.querySelector('.menu__list'),
-    menuItem = document.querySelectorAll('.menu__item'),
-    hamburger = document.querySelector('.header__hamburger');
+    const menu = document.querySelector('.header__navigation'),
+    menuItem = document.querySelectorAll('.menu__link'),
+    hamburger = document.querySelector('.header__hamburger'),
+    lines = document.querySelectorAll('.header__hamburger-item');
+    
 
     hamburger.addEventListener('click', () => {
-        hamburger.classList.toggle('header__hamburger-item--active');
-        menu.classList.toggle('menu__list--active');
-    });
+        hamburger.classList.toggle('header__hamburger--active');
+        menu.classList.toggle('header__navigation--active');
+        lines.forEach((lines) => {
+            lines.classList.toggle('header__hamburger--active-item');
+        });
+    })
 
     menuItem.forEach(item => {
         item.addEventListener('click', () => {
-            hamburger.classList.toggle('header__hamburger-item');
-            menu.classList.toggle('menu__list');
+            hamburger.classList.toggle('header__hamburger--active');
+            menu.classList.toggle('header__navigation--active');
+            lines.classList.toggle('header__hamburger--active-item');
         })
     })
 })
+
+
+
+/* header__hamburger--active-item */
