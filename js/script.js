f4d037fc4a78b8b4b7f7687dabd5f72528c31ce1
Egor K <digital.zeus@mail.ru>
2021-01-26 23:26:56 +0300

    make arrow go top and hidden

--- js/script.js
+++ js/script.js
@@ -1,77 +1,101 @@
-'use strict'
+"use strict";
 
-window.addEventListener('DOMContentLoaded', () => {
-    const menu = document.querySelector('.header__navigation'),
-    menuItem = document.querySelectorAll('.menu__link'),
-    hamburger = document.querySelector('.header__hamburger'),
-    lines = document.querySelectorAll('.header__hamburger-item');
-    
+// * ====== Hamburger active class toggle
 
-    hamburger.addEventListener('click', () => {
-        hamburger.classList.toggle('header__hamburger--active');
-        menu.classList.toggle('header__navigation--active');
-        menuItem.forEach((menuItem) => {
-        menuItem.classList.toggle('header__hamburger--active-item');
-        });
-        lines.forEach((lines) => {
-            lines.classList.toggle('header__hamburger--active-item');
-        });
-    })
+window.addEventListener("DOMContentLoaded", () => {
+  const menu = document.querySelector(".header__navigation"),
+    menuItem = document.querySelectorAll(".menu__link"),
+    hamburger = document.querySelector(".header__hamburger"),
+    lines = document.querySelectorAll(".header__hamburger-item");
 
-    menuItem.forEach(item => {
-        item.addEventListener('click', () => {
-            hamburger.classList.toggle('header__hamburger--active');
-            menu.classList.toggle('header__navigation--active');
-            lines.forEach((lines) => {
-            lines.classList.toggle('header__hamburger--active-item');
-            })
-        })
-    })
-})
+  hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("header__hamburger--active");
+    menu.classList.toggle("header__navigation--active");
+    menuItem.forEach((menuItem) => {
+      menuItem.classList.toggle("header__hamburger--active-item");
+    });
+    lines.forEach((lines) => {
+      lines.classList.toggle("header__hamburger--active-item");
+    });
+  });
 
+  menuItem.forEach((item) => {
+    item.addEventListener("click", () => {
+      hamburger.classList.toggle("header__hamburger--active");
+      menu.classList.toggle("header__navigation--active");
+      lines.forEach((lines) => {
+        lines.classList.toggle("header__hamburger--active-item");
+      });
+    });
+  });
+});
 
+// * ====== Cart count
 
+let buyButtons = document.querySelectorAll(".btn--white");
 
-
-let buyButtons = document.querySelectorAll('.btn--white');
-
-let cart_count = document.getElementById('cart_count');
+let cart_count = document.getElementById("cart_count");
 
 function addToCart() {
-    cart_count.innerText = Number(cart_count.innerText) + 1
-};
+  cart_count.innerText = Number(cart_count.innerText) + 1;
+}
 
-// buyButtons[0].addEventListener
-
-for(let i = 0; i < buyButtons.length; i++) {
-    buyButtons[i].addEventListener('click', addToCart)
-};
+for (let i = 0; i < buyButtons.length; i++) {
+  buyButtons[i].addEventListener("click", addToCart);
+}
 
 
 
 
-// * JS Scroll 
+// * JS Scroll
 
-    let anchors = document.querySelectorAll('a[href*="#"]');
+let anchors = document.querySelectorAll('a[href*="#"]');
 
-/*     for(let i=0; i< anchors.length; i++) {
-        anchors[i].addEventListener
-    } */
+for (let anchor of anchors) {
+  anchor.addEventListener("click", function (event) {
+    event.preventDefault();
+    let blockId = anchor.getAttribute("href");
+    document.querySelector(blockId).scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  });
+}
 
-    for(let anchor of anchors) {
-        anchor.addEventListener('click', function(event) {
-            event.preventDefault()
-            let blockId = anchor.getAttribute('href')
-            document.querySelector(blockId).scrollIntoView({
-                behavior: 'smooth',
-                block: 'start'
-            })
-        })
+// * Smooth scroll btn top
+
+
+let goTopBtn = document.querySelector('.gotop');
+
+
+
+/* window.scroll(function(){
+    if (window.scrollTop > 100) {
+        goTopBtn.fadeIn();
+    } else {
+        goTopBtn.fadeOut();
     }
+})
+ */
 
+goTopBtn.onclick = function() {
+    window.scrollTo(pageXOffset, 0);
+    // после scrollTo возникнет событие "scroll", так что стрелка автоматически скроется
+  };
 
+  window.addEventListener('scroll', function() {
+    goTopBtn.hidden = (pageYOffset < document.documentElement.clientHeight);
+  });
 
-
+// document.onload = (function() {
+//     window.scroll(function() {
+//         if (window.scrollTop() > 1600) {
+//             goTopBtn.fadeIn();
+//         } else {
+//             goTopBtn.fadeOut();
+//         }
+//     });
+// })
 
 
 
